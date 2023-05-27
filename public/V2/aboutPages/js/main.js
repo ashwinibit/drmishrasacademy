d27deea445d08bd8595e4ab5946c0fdb8a75a70f
ashwinibit <ashwinikumarrbhilai@gmail.com>
2023-05-28 04:30:47 +0530

    chnge to my page

--- public/V2/aboutPages/js/main.js
+++ public/V2/aboutPages/js/main.js
@@ -3,3 +3,25 @@ const themeBtn = document.querySelector('.theme-btn');
         let element = document.body;
         element.classList.toggle('light-mode')
     })
+
+
+function addPublishedPapers(){
+    const t1 = document.getElementById("papersPublished");
+    
+    var timelineDiv = document.createElement("div");
+    timelineDiv.className = "timeline";
+
+    var timelineItem = document.createElement("div");
+    timelineItem.className = "timeline-item";
+
+    var timeLineIcon = document.createElement("div");
+    timeLineIcon.className = "tl-icon";
+
+    timeLineIcon.innerHTML = '<i class="fas fa-briefcase"></i>';
+
+    timelineItem.appendChild(timeLineIcon);
+    timelineDiv.appendChild(timelineItem);
+    t1.appendChild(timelineDiv);
+}
+
+addPublishedPapers()
